5cc2f36c6d88a2eb9edf2165bb01edab16553531
Vicente Oliva <vicente_Oliva@hotmail.com>
2015-01-12 12:18:28 +0100

    Initial Commit

---
+++ StarWars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarWars
+//
+//  Created by Vicente Oliva de la Serna on 12/1/15.
+//  Copyright (c) 2015 Vicente Oliva de la Serna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
